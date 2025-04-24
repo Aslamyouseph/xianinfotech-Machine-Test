53cd8b85e97ad0a1fccefebbe444dc082f90a4a8
Aslam youseph <aslamyouseph@gmail.com>
2025-04-24 19:15:23 +0530

    Admin Page

---
+++ client/src/AdminCompounts/AdminPage/AdminHome.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import HomeDesign from "../../AdminCompounts/AdminHome/AdminHome";
+function AdminHome() {
+  return (
+    <div>
+      <HomeDesign /> {/*This is used to import the home page design*/}
+    </div>
+  );
+}
+
+export default AdminHome;
--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import React, { useEffect, useState } from "react";
 import axios from "axios"; //This us used to connect the frontend to the backend
 import { Routes, Route, Link, useNavigate } from "react-router-dom"; // This is used to enable routing.
+//TODO: User Pages Imported
 import Home from "./UserCompounts/userPage/home";
 import Login from "./UserCompounts/userPage/login";
 import Signup from "./UserCompounts/userPage/signup";
 import Profile from "./UserCompounts/userPage/profile";
 import EditProfile from "./UserCompounts/userPage/editProfile";
 import FeedBack from "./UserCompounts/userPage/FeedBack";
+//TODO:Admin Pages Imported
+import AdminHome from "./AdminCompounts/AdminPage/AdminHome";
 function App() {
   const [data, setData] = useState(null);
 
@@ -26,6 +29,7 @@ function App() {
   return (
     <div>
       <Routes>
+        {/* //TODO: User Pages */}
         {/*creating a route for home page*/}
         <Route path="/" element={<Home />} />
         {/*creating a route for edit profile */}
@@ -37,7 +41,9 @@ function App() {
         {/*creating a route signup*/}
         <Route path="/signup" element={<Signup />} />{" "}
         {/*creating a route chatApp page*/}
-        <Route path="/FeedBack" element={<FeedBack />} />{" "}
+        <Route path="/FeedBack" element={<FeedBack />} />
+        {/* //TODO: Admin Pages */}
+        <Route path="/AdminHome" element={<AdminHome />} />
       </Routes>
     </div>
   );
